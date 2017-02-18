3464c7f86d1fac592daf6d6c578900be98401986
Guillermo Manzato <manzato@gmail.com>
2017-02-18 14:57:15 -0300

    Minor adjustments

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 This is mostly because I'm lazy. It installs most of the things I normally install. 
 
+Not the most original idea, and I stole it from Tulku :-)
+
+Usage:
+
+```
+curl https://raw.githubusercontent.com/manzato/setup/master/bringup.sh && chmod u+x bringup.sh && sudo ./bringup.sh
+```
--- bringup.sh
+++ bringup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-HEADLESS=0
-DEV=0
-
 function ask {
-  Q=$1
-  VAR=$2
+  Q=${1}
+  VAR=${2}
   while true ; do
     echo "${Q} [y/n]"
     read -sn 1 a
@@ -20,6 +17,10 @@ function ask {
   done 
 }
 
+
+HEADLESS=0
+DEV=0
+
 ask "Is this machine headless?" HEADLESS
 ask "Is this a dev machine ?" DEV
 
@@ -30,7 +31,7 @@ echo "export EDITOR=vi" >> ~/.bashrc
 
 if [ ${DEV} -eq 1 ] ; then
   sudo apt-get install -y tmux git 
-  
+    
   git config --global user.email "manzato@gmail.com"
   git config --global user.name "Guillermo Manzato"
 
@@ -40,7 +41,7 @@ if [ ${DEV} -eq 1 ] ; then
     sudo apt-get install -f
   fi
 fi
-
+  
 if [ ${HEADLESS} -eq 0 ] ; then
   sudo apt-get install -y indicator-multiload indicator-cpufreq
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
